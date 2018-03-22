f9c9947e382cc87e3b8376ff5c937d0c4ff630af
martin nielsen fig <martin.fig@hotmail.com>
2018-03-23 00:35:03 +0100

    makes post to get id

--- trackerAPi/Controllers/TrackerController.cs
+++ trackerAPi/Controllers/TrackerController.cs
@@ -1,8 +1,12 @@
 ﻿using Core.DomainModel;
 using Core.DomainServices;
 using Infrastructure.Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -24,13 +28,13 @@ namespace trackerAPi.Controllers
         }
 
         [EnableCors(origins: "*", headers: "*", methods: "*")]
-        public IOrderedEnumerable<Manga> Get(string sortOrder = "new_chapter")
+        public IOrderedEnumerable<Manga> Get(string sortOrder = "new_chapter_desc")
         {
-           
-                list = _listRepository.Get().Single(s => s.Owner == "948395601842199");
-           
-   
-                IOrderedEnumerable<Manga> mangas;
+
+            list = _listRepository.Get().Single(s => s.Owner == "948395601842199");
+
+
+            IOrderedEnumerable<Manga> mangas;
             switch (sortOrder)
             {
                 case "name_desc":
@@ -47,6 +51,33 @@ namespace trackerAPi.Controllers
                     break;
             }
             return mangas;
+        }
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [HttpPost]
+        public void CheckForNew()
+        {
+            list = _listRepository.Get().Single(s => s.Owner == "948395601842199");
+            var listofMangas = list.Mangas;
+            List<Manga> todaysshows = new List<Manga>();
+
+
+            foreach (var manga in listofMangas)
+            {
+                WebRequest request = WebRequest.Create("https://api.mangarockhd.com/query/web400/mrs_quick_search?country=Denmark");
+                request.Method = "POST";
+                request.ContentLength = manga.Name.Length;
+                using (var dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(Encoding.ASCII.GetBytes(manga.Name), 0, manga.Name.Length);
+                }
+
+                WebResponse response = request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                string text = reader.ReadToEnd();
+
+            }
         }
     }
 }
